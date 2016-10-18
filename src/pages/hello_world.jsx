2b582b79c3b4eac9d966c8cb24f510c7441d3772
sh <siegby@yahoo.com>
2016-10-18 10:11:53 -0700

    removed comments

--- src/pages/hello_world.jsx
+++ src/pages/hello_world.jsx
@@ -6,17 +6,6 @@ import * as actionCreators from '../redux/action_creators'
 
 class HelloWorld extends Component {
 
-    // constructor(props) {
-    //     super(props)
-    //
-    //     // this.addNumber = this.addNumber.bind(this)
-    //
-    //     // this is local to this component
-    //     // this.state = {   
-    //     //     number: 1
-    //     // }
-    //
-    // }
 
 
 
