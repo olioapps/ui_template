a07b6c1b6530fb2cb8fe33d3037733a701d3c6cf
sh <siegby@yahoo.com>
2016-10-18 15:14:30 -0700

    In newpage added ability to change greeting from text input. saved local state onChange and then saved to store when save button is pushed.

--- src/pages/newpage.jsx
+++ src/pages/newpage.jsx
@@ -5,18 +5,42 @@ import * as actionCreators from '../redux/action_creators'
 
 
 class NewPage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.setGreeting = this.setGreeting.bind(this);
+
+        this.state = {
+            greeting: 'Hello Olio',
+        }
+    }
+
     static get contextTypes() {
         return { router: React.PropTypes.object.isRequired }
     }
 
+    setGreeting(e) {
+        this.setState({ greeting: e.target.value })
+    }
+
+
     render() {
         return (
             <div>
-                <input type="text" onChange={e => this.props.setGreeting(e.target.value)} />
 
                 <div>Yay a new page</div>
+                <h4>{this.state.greeting}</h4>
+
+                <h6>{this.props.hello.greeting}</h6>
+
+                <h3>Enterer New Greeting: </h3>
+
+                <input type="text" onChange={this.setGreeting} />
+
                 <button onClick={() => this.context.router.goBack()}>Go back</button>
-                <button onClick={() => this.context.router.push('/')}>Go to Hello</button>
+
+                <button onClick={() => this.props.setGreeting(this.state.greeting)}>Save</button>
+
             </div>
         )
     }
