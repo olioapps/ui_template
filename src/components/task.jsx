899966bc99df60adfceef82897616d0bc703b60a
sh <siegby@yahoo.com>
2016-10-20 13:37:12 -0700

    added task_list component, refactored task, task_list components inside of TodoApp  component.

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -4,31 +4,21 @@ import * as actionCreators from '../redux/action_creators'
 
 
 class Task extends Component {
-    constructor(props) {
-        super(props)
-        
-        this.nextPage = this.nextPage.bind(this)
 
-    }
-    
     static get contextTypes() {
         return { router: React.PropTypes.object.isRequired }
     }
 
-    nextPage() {
-        this.context.router.push('/editGreeting')
-    }
 
     render() {
         return (
             <div>
 
                 <input type="checkbox"/>
-                <span>Task</span>
+                <span>Task </span>
                 <button> Edit </button>
                 <button> Delete </button>
-                <br/>
-                <button>New Task Button</button>
+
             </div>
         )
     }
--- src/components/task_list.jsx
+++ src/components/task_list.jsx
@@ -1,26 +1,32 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import * as actionCreators from '../redux/action_creators'
-import TaskList from '../components/task_list'
+import Task from './task'
 
-class TodoApp extends Component {
+
+
+class TaskList extends Component {
+
 
     static get contextTypes() {
         return { router: React.PropTypes.object.isRequired }
     }
-
-  
+    
 
     render() {
         return (
             <div>
-                <h1>To Do App...</h1>
-                <TaskList />
 
+                <h3>A List</h3>
+                <Task />
+                <Task />
+                <Task />
+                <br/>
+                <button>New Task Button</button>
 
             </div>
         )
     }
 }
 
-export default connect( state => state.toJSON(), actionCreators )(TodoApp)
+export default connect( state => state.toJSON(), actionCreators )(TaskList)
