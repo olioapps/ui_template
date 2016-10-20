d77b1013c1588bb5f15461d6e860ae8baa2aaafa
sh <siegby@yahoo.com>
2016-10-20 13:57:24 -0700

    added a side_menu component + basic edit and new list buttons.

--- src/components/side_menu.jsx
+++ src/components/side_menu.jsx
@@ -1,28 +1,29 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import * as actionCreators from '../redux/action_creators'
-import TaskList from '../components/task_list'
-import SideMenu from '../components/side_menu'
 
-class TodoApp extends Component {
+
+
+class SideMenu extends Component {
+
 
     static get contextTypes() {
         return { router: React.PropTypes.object.isRequired }
     }
-
-  
+    
 
     render() {
         return (
             <div>
-                <h1>To Do App...</h1>
-                <SideMenu />
-                <TaskList />
 
+                <h3>User</h3>
+                <h6>My lists... </h6>
 
+                <button> Edit </button>
+                <button> New List </button>
             </div>
         )
     }
 }
 
-export default connect( state => state.toJSON(), actionCreators )(TodoApp)
+export default connect( state => state.toJSON(), actionCreators )(SideMenu)
