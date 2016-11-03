c7bed094bc444a97de991b127d357f3cbd2bdb52
Jonathan Walz <jon.walz.music@gmail.com>
2016-11-03 11:09:05 -0700

    Reformatted

--- src/components/side_menu.jsx
+++ src/components/side_menu.jsx
@@ -6,7 +6,6 @@ import SideMenuOptions from './side_menu_options'
 import HelpText from './help_text'
 
 
-
 class SideMenu extends Component {
     constructor(props) {
         super(props)
@@ -19,7 +18,6 @@ class SideMenu extends Component {
         this.toggleAddMode = this.toggleAddMode.bind(this)
         this.revealOptions = this.revealOptions.bind(this)
         this.newListElement = this.newListElement.bind(this)
-
 
 
         this.state = {
@@ -53,7 +51,9 @@ class SideMenu extends Component {
 
 
     toggleAddMode() {
-        if(this.state.editListMode) { this.toggleEditListMode() }
+        if (this.state.editListMode) {
+            this.toggleEditListMode()
+        }
         this.setState({addMode: !this.state.addMode})
     }
 
@@ -88,12 +88,12 @@ class SideMenu extends Component {
         return (
             <div>
                 <div className="btnContainer">
-                        <input autoFocus type="text" placeholder="Enter new list name" value={this.state.listName}
-                            onChange={this.newList} onKeyPress={this.handleKeyPress} />
-                        <button onClick={this.saveList}><i className="fa fa-check" /></button>
-                        <button onClick={this.toggleAddMode}><i className="fa fa-times" /></button>
-                        <br/>
-                     </div>
+                    <input autoFocus type="text" placeholder="Enter new list name" value={this.state.listName}
+                           onChange={this.newList} onKeyPress={this.handleKeyPress}/>
+                    <button onClick={this.saveList}><i className="fa fa-check"/></button>
+                    <button onClick={this.toggleAddMode}><i className="fa fa-times"/></button>
+                    <br/>
+                </div>
                 { this.state.showHelp ? <HelpText /> : null }
             </div>
         )
@@ -116,10 +116,10 @@ class SideMenu extends Component {
                     revealOptionsBool={this.state.revealOptionsBool}
                     revealOptions={this.revealOptions}
                     toggleAddMode={this.toggleAddMode}
-                    addMode={this.state.addMode} />
+                    addMode={this.state.addMode}/>
                 </span>
                 <ul id="list">
-                {lists}
+                    {lists}
                 </ul>
 
                 {this.state.addMode
@@ -129,7 +129,6 @@ class SideMenu extends Component {
 
             </div>
         )
-        
     }
 }
 
