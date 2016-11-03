e5ac6f36e1471dcd2c22d91a4492f5c095c0ddd3
Jonathan Walz <jon.walz.music@gmail.com>
2016-11-03 16:25:11 -0700

    Removed unused import

--- src/components/task_list.jsx
+++ src/components/task_list.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react'
-import ReactDOM from 'react-dom'
 import {connect} from 'react-redux'
 import * as actionCreators from '../redux/action_creators'
 import Task from './task'
