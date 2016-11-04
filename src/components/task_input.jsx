aa139ec95e90b3f113ae92feede9096112cc8f37
Jonathan Walz <jon.walz.music@gmail.com>
2016-11-03 17:28:01 -0700

    Moved task input into separate component

---
+++ src/components/task_input.jsx
@@ -0,0 +1,79 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import Validate from './validate'
+import * as actionCreators from '../redux/action_creators'
+
+class TaskInput extends Component {
+    constructor(props) {
+        super(props)
+
+        this.clearTask = this.clearTask.bind(this)
+        this.handleKeyPress = this.handleKeyPress.bind(this)
+        this.submitTask = this.submitTask.bind(this)
+        this.setTask = this.setTask.bind(this)
+
+        this.state = {
+            showHelp: false,
+            taskName: '',
+        }
+    }
+
+    submitTask() {
+        this.setState({showHelp: false})
+
+        if (this.state.taskName !== '') {
+            this.props.saveTask(this.props.currentListId, this.state.taskName)
+            this.clearTask()
+        } else {
+            this.setState({showHelp: true})
+        }
+
+    }
+
+    saveTask() {
+        this.props.addTask(this.props.currentListId, this.state.taskName)
+        this.clearTask()
+    }
+
+    setTask(event) {
+        const text = event.target.value
+        this.setState({taskName: text})
+    }
+
+    clearTask() {
+        this.setState({taskName: ''})
+    }
+
+    handleKeyPress(e) {
+        if (e.key === 'Enter') {
+            this.saveTask()
+        }
+    }
+
+    render() {
+        return (
+            <div className="newTaskContainer">
+                <input id="task_input"
+                       autoFocus
+                       type="text"
+                       placeholder="Enter new task"
+                       value={this.state.taskName}
+                       onChange={this.setTask}
+                       onKeyPress={this.handleKeyPress}/>
+                <div className="btnContainer">
+                    <button
+                        onClick={this.saveTask}>
+                        <i className="fa fa-check"/>
+                    </button>
+                    <button
+                        onClick={this.clearTask}>
+                        <i className="fa fa-times"/>
+                    </button>
+                </div>
+                { this.state.showHelp ? <Validate /> : null }
+            </div>
+        )
+    }
+}
+
+export default connect(state => state.toJSON(), actionCreators)(TaskInput)
--- src/components/task_list.jsx
+++ src/components/task_list.jsx
@@ -2,72 +2,9 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import * as actionCreators from '../redux/action_creators'
 import Task from './task'
-import Validate from './validate'
-
+import TaskInput from './task_input'
 
 class TaskList extends Component {
-    constructor(props) {
-        super(props)
-
-        this.setTask = this.setTask.bind(this)
-        this.saveTask = this.saveTask.bind(this)
-        this.clearTask = this.clearTask.bind(this)
-        this.handleKeyPress = this.handleKeyPress.bind(this)
-        this.renderTaskInput = this.renderTaskInput.bind(this)
-
-        this.state = {
-            taskName: '',
-            showHelp: false,
-        }
-    }
-
-    setTask(event) {
-        const text = event.target.value
-        this.setState({taskName: text})
-        this.setState({showHelp: false})
-    }
-
-    saveTask() {
-        if (this.state.taskName !== '') {
-            this.props.addTask(this.props.currentListId, this.state.taskName)
-            this.clearTask()
-        } else {
-            this.setState({showHelp: true})
-        }
-    }
-
-    handleKeyPress(e) {
-        if (e.key === 'Enter') {
-            this.saveTask()
-        }
-    }
-
-    clearTask() {
-        this.setState({taskName: ''})
-    }
-
-    renderTaskInput() {
-        return (
-            <div className="newTaskContainer">
-                <input id="task_input"
-                       ref="taskInput" type="text"
-                       placeholder="Enter new task"
-                       value={this.state.taskName}
-                       onChange={this.setTask}
-                       onKeyPress={this.handleKeyPress}/>
-                <div className="btnContainer">
-                    <button
-                        onClick={this.saveTask}>
-                        <i className="fa fa-check"/>
-                    </button>
-                    <button
-                        onClick={this.clearTask}>
-                        <i className="fa fa-times"/>
-                    </button>
-                </div>
-            </div>
-        )
-    }
 
     render() {
 
@@ -84,10 +21,9 @@ class TaskList extends Component {
                 {listOfTasks}
 
                 {this.props.catalog.length !== 0
-                    ? this.renderTaskInput()
+                    ? <TaskInput saveTask={this.saveTask} addTask={this.props.addTask}/>
                     : <h2>Please Create A List</h2>
                 }
-                { this.state.showHelp ? <Validate /> : null }
 
             </div>
         )
